78ba97619eef339ba2073b9c66eaf3ebeb3d2bb2
RhinosF1 <rhinosf1@gmail.com>
2020-05-12 08:23:59 +0000

    Make Script use a config file.
    Introduces a weekly and monthly parameter when running the script.
    
    A seperate file can be specfied for manual runs
    
    Also introduces a general config file for phaburl, apikey and sender email.
    
    
    Change-Id: I9ed06425b46114678162152bed5a3307f94bea12

--- script.py
+++ script.py
@@ -3,37 +3,57 @@ import json
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-
-file = open('<config file>', 'r')
-for line in file:
-    info = line.split(',')
-    data = {
-      'api.token': '<redacted>',
-      'queryKey': info[1]
-    }
-
-    response = requests.post('https://phabricator.wikimedia.org/api/maniphest.search', data=data)
-    response = response.json()
-    result = response["result"]
-    data = result["data"]
-    x = 0
-    output = ''
-    while x < len(data):
-        parse = data[x]
-        description = parse["fields"]
-        description = description["name"]
-        output = output + "\nhttps://phabricator.wikimedia.org/T" + str(parse["id"]) + " - " + str(description)
-        output = str(output)
-        x = x + 1
-    msg = MIMEMultipart()
-    msg["Subject"] = "Phabricator Search Alert"
-    msg["From"] = "tools.phabsearchemail@tools.wmflabs.org"
-    msg["To"] = info[2]
-    body = "This is your automated search alert from Wikimedia Phabricator \n " + output
-    msg.attach(MIMEText(body, 'plain'))
-
-    smtp = smtplib.SMTP("localhost")
-    smtp.sendmail(msg["From"], msg["To"], msg.as_string())
-    smtp.quit()
-    print ("Successfully sent email")
-file.close()
+import sys
+def run(config):
+  file = open('config.csv', 'r')
+  apikey = ''
+  phaburl = 'phabricator.wikimedia.org'
+  sender = ''
+  for line in file:
+    configdata = line.split(',')
+    if configdata[1] == 'apikey':
+      apikey = configdata[2]
+    if configdata[1] == 'phaburl':
+      phaburl = configdata[2]
+    if configdata[1] == 'sender':
+      sender = configdata[2]
+  file = open(config, 'r')
+  for line in file:
+      info = line.split(',')
+      data = {
+        'api.token': apikey,
+        'queryKey': info[1]
+      }
+      response = requests.post('https://' + phaburl + '/api/maniphest.search', data=data)
+      response = response.json()
+      result = response["result"]
+      data = result["data"]
+      x = 0
+      output = ''
+      while x < len(data):
+          parse = data[x]
+          description = parse["fields"]
+          description = description["name"]
+          output = output + "\nhttps://" + phaburl + "/T" + str(parse["id"]) + " - " + str(description)
+          output = str(output)
+          x = x + 1
+      msg = MIMEMultipart()
+      msg["Subject"] = "Phabricator Search Alert"
+      msg["From"] = sender
+      msg["To"] = info[2]
+      body = "This is your automated search alert from Phabricator \n " + output
+      msg.attach(MIMEText(body, 'plain'))
+      smtp = smtplib.SMTP("localhost")
+      smtp.sendmail(msg["From"], msg["To"], msg.as_string())
+      smtp.quit()
+      print ("Successfully sent email")
+  file.close()
+try:
+  if sys.argv[1] == 'weekly':
+    run('weekly.csv')
+  elif sys.argv[1] == 'monthly':
+    run('monthly.csv')
+  else:
+    run(sys.argv[1])
+except IndexError:
+ print("No command specified.")
